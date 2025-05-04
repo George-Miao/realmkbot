9e8d5c3f7276836d69274f9600b307cf07c55190
George Miao <gm@miao.dev>
2025-05-04 16:10:00 +0800

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Compio dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              glib
+              openssl
+              pkg-config
+              (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+            ];
+          };
+        }
+    );
+}
